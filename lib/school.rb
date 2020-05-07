4a0758535a80dba9bc760f8a2d412aead985bdd2
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-07 23:04:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -28,7 +28,7 @@ class School
   def sort
     sorted_roster = {}
     @roster.each do |key, values|
-      sorted_roster[key] = value
+      sorted_roster[key] = values
     sorted_roster << @roster
     sorted_roster
   end
