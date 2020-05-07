9d94eb9dbb25402a6aa4f4d15da43afa6e8c7377
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-07 22:38:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+     if @roster[grade]
+          @roster[grade] << name 
+    else
+      @roster[grade] = []
+        @roster[grade] << name
+    end
+    @roster
+  end
+  
+  def grade(grade)
+    if @roster[grade]
+      @roster[grade]
+    end
+  end
+  
+  def sort
+  
+  end
+  
+end
+
+# roster {
+#   {10 => ["Student1", "Student2"]},
+#   {9 => ["Student1", "Student2"],
+#   }
